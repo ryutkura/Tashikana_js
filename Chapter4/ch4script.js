2413d62ee72752b569223f6354e75acf33c05c31
ryutkura <ryutkura@gmail.com>
2023-10-07 15:32:14 +0900

    フォームの勉強が完了しました。

--- Chapter4/ch4script.js
+++ Chapter4/ch4script.js
@@ -1,4 +1,6 @@
 // JavaScript外部ファイル
 document.getElementById('form').onsubmit = function(){
-    console.log('クリックされました');
+    var search = document.getElementById('form').word.value;
+    document.getElementById('output').textContent = '『'+ search+'』の検索中．．．';
+    return false;
 };
