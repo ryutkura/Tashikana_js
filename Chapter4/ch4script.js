7cf20a1fc226987aee5437a4c875652a64a32371
ryutkura <ryutkura@gmail.com>
2023-10-07 15:54:44 +0900

    4章の学習を終了します

--- Chapter4/ch4script.js
+++ Chapter4/ch4script.js
@@ -1,6 +1,22 @@
 // JavaScript外部ファイル
-document.getElementById('form').onsubmit = function(){
-    var search = document.getElementById('form').word.value;
-    document.getElementById('output').textContent = '『'+ search+'』の検索中．．．';
-    return false;
-};
+// document.getElementById('form').onsubmit = function(){
+//     var search = document.getElementById('form').word.value;
+//     document.getElementById('output').textContent = '『'+ search+'』の検索中．．．';
+//     return false;
+// };
+var now = new Date();
+var year = now.getFullYear();
+var month = now.getMonth();
+var date = now.getDate();
+var hour = now.getHours();
+var min = now.getMinutes();
+var ampm = '';
+if(hour < 12){
+    ampm = 'a.m.';
+}
+else{
+    ampm = 'p.m.';
+}
+
+var output = year + '/' + (month+1) + '/' +date +' '+(hour%12)+':'+ min + ampm;
+document.getElementById('time').textContent = output;
