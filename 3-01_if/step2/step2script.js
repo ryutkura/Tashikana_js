b306bcf8d9b165e8dfb033dc1c50d4280358fd53
ryutkura <ryutkura@gmail.com>
2023-10-07 13:14:07 +0900

    for文の確認が終わりました。

--- 3-01_if/step2/step2script.js
+++ 3-01_if/step2/step2script.js
@@ -1,10 +1,13 @@
 //外部JavaScriptファイル
-var hour = new Date().getHours();
+// var hour = new Date().getHours();
 
-if(hour >= 19 && hour < 21){
-    window.alert('お弁当30%OFF！');
-}else if(hour === 9 || hour ===15){
-    window.alert('お弁当1個買ったら1個おまけ！');
-}else{
-    window.alert('お弁当はいかがでしょうか？');
+// if(hour >= 19 && hour < 21){
+//     window.alert('お弁当30%OFF！');
+// }else if(hour === 9 || hour ===15){
+//     window.alert('お弁当1個買ったら1個おまけ！');
+// }else{
+//     window.alert('お弁当はいかがでしょうか？');
+// }
+for(var i = 1;i <= 10; i++){
+    console.log(i + "枚");
 }
