73dc9e8036832a956db051fc95b3fb9997f29048
ryutkura <ryutkura@gmail.com>
2023-10-07 13:33:18 +0900

    関数の基礎を覚えました HTMLに出力します

--- 3-01_if/step2/step2script.js
+++ 3-01_if/step2/step2script.js
@@ -11,16 +11,23 @@
 // for(var i = 1;i <= 10; i++){
 //     console.log(i + "枚");
 // }
-var enemy = 100;
-var attack;
-var count =0;
+//while文の内容
+// var enemy = 100;
+// var attack;
+// var count =0;
 
-window.alert('戦闘スタート！');
-while(enemy > 0){
-    attack = Math.floor(Math.random() * 30) + 1;
-    console.log('モンスターに'+attack+'のダメージ！');
-    enemy -=  attack;
-    count++;
+// window.alert('戦闘スタート！');
+// while(enemy > 0){
+//     attack = Math.floor(Math.random() * 30) + 1;
+//     console.log('モンスターに'+attack+'のダメージ！');
+//     enemy -=  attack;
+//     count++;
+// }
+// console.log('モンスターを倒した！');
+// console.log(count);
+var total = function(price){
+    var tax = 0.1;
+    return price + price * tax;
 }
-console.log('モンスターを倒した！');
-console.log(count);
+
+console.log('コーヒーメイカーの値段は'+total(8000)+'円(税込)です。');
