7bb303a44dd0fef863956b438042f53c99d4dbe7
ryutkura <ryutkura@gmail.com>
2023-10-08 11:09:28 +0900

    ページ遷移のJSを記述しました。

--- Chapter5/ch5script.js
+++ Chapter5/ch5script.js
@@ -1,34 +1,38 @@
-//外部ファイルだよ
-var countdown = function(due){
-    var now = new Date();
+// //外部ファイルだよ
+// var countdown = function(due){
+//     var now = new Date();
 
-    var rest = due.getTime() - now.getTime();
-    var sec = Math.floor(rest / 1000 % 60);
-    var min = Math.floor(rest / 1000 / 60) % 60;
-    var hours = Math.floor(rest / 1000 / 60 / 60) % 24;
-    var days = Math.floor(rest / 1000 / 60 / 60 /24);
-    var count = [days, hours, min, sec];
+//     var rest = due.getTime() - now.getTime();
+//     var sec = Math.floor(rest / 1000 % 60);
+//     var min = Math.floor(rest / 1000 / 60) % 60;
+//     var hours = Math.floor(rest / 1000 / 60 / 60) % 24;
+//     var days = Math.floor(rest / 1000 / 60 / 60 /24);
+//     var count = [days, hours, min, sec];
 
-    return count;
+//     return count;
+// }
+
+// var goal = new Date(2024,1,13);
+// // goal.setHours(23);
+// // goal.setMinutes(59);
+// // goal.setSeconds(59);
+
+// var recalc = function(){
+// var counter = countdown(goal);
+// // var time = counter[1] + '時間' + counter[2] + '分' + counter[3] + '秒';
+// // document.getElementById('timer').textContent = time;
+// document.getElementById('day').textContent = counter[0];
+// document.getElementById('hour').textContent = counter[1];
+// document.getElementById('min').textContent = counter[2];
+// document.getElementById('sec').textContent = counter[3];
+// refresh();
+// }
+
+// var refresh = function(){
+//     setTimeout(recalc,5000);
+//     //↑上の2個目のパラメータは待ち時間。ミリ秒なので1000で1秒5000で5秒
+// }
+// recalc();
+document.getElementById('form').select.onchange = function(){
+    location.href = document.getElementById('form').select.value;
 }
-
-var goal = new Date(2024,1,13);
-// goal.setHours(23);
-// goal.setMinutes(59);
-// goal.setSeconds(59);
-
-var recalc = function(){
-var counter = countdown(goal);
-// var time = counter[1] + '時間' + counter[2] + '分' + counter[3] + '秒';
-// document.getElementById('timer').textContent = time;
-document.getElementById('day').textContent = counter[0];
-document.getElementById('hour').textContent = counter[1];
-document.getElementById('min').textContent = counter[2];
-document.getElementById('sec').textContent = counter[3];
-refresh();
-}
-
-var refresh = function(){
-    setTimeout(recalc,1000);
-}
-recalc();
